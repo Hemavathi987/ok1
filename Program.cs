8211b564e8c137eccf49a0125f303a51c7208646
Hemavathi987 <136674928+Hemavathi987@users.noreply.github.com>
2024-07-01 11:21:47 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+IConfiguration configuration;
+
+configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+builder.Services.AddDbContext<IpDbfirstContext>
+
+    (option => option.UseSqlServer(configuration.GetConnectionString("Students123ConnectionString")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
